77fbfe264f08355b3c0d8ceeb11939ae6fee5e57
liuyong <564447023@qq.com>
2020-02-15 02:07:57 +0800

    Updates

---
+++ precache-manifest.b8174f47c5e7a06ad331724fafa7d570.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a146861fca3e96600a19fe231d96e005",
+    "url": "/tetris-react/index.html"
+  },
+  {
+    "revision": "1724ef38a3a6c2872059",
+    "url": "/tetris-react/static/css/main.dccbf2d2.chunk.css"
+  },
+  {
+    "revision": "4775aa1b68cf0aa64649",
+    "url": "/tetris-react/static/js/2.20da1521.chunk.js"
+  },
+  {
+    "revision": "1724ef38a3a6c2872059",
+    "url": "/tetris-react/static/js/main.e6ccf5a6.chunk.js"
+  },
+  {
+    "revision": "4216133b82bd6e6a9135",
+    "url": "/tetris-react/static/js/runtime~main.88e267e5.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/tetris-react/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/tetris-react/static/media/bg.2810fe6b.png"
+  }
+]);
